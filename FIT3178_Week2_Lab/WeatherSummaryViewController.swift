86db8ff60de0f54f827e7cb96d6b57f92b4def45
Thanh Son Truong <ttru0037@student.monash.edu>
2025-08-13 01:04:42 +1000

    Link IBOutlets and prepare segue

---
+++ FIT3178_Week2_Lab/WeatherSummaryViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherSummaryViewController.swift
+//  FIT3178_Week2_Lab
+//
+//  Created by Son Truong on 6/8/2025.
+//
+
+import UIKit
+
+class WeatherSummaryViewController: UIViewController {
+
+    @IBOutlet weak var summaryButton: UIButton!
+}
